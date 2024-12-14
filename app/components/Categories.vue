2abb991fe1640b3f73713dfddf7ee5bd0082cd90
Anton <antklim89@mail.ru>
2024-12-14 20:21:41 +0300

    feat(components): updated Categories
    add hr, make button fixed

--- app/components/Categories.vue
+++ app/components/Categories.vue
@@ -8,36 +8,40 @@ const selectedCategory = ref<CategoryId | null>(null);
 </script>
 
 <template>
-  <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 my-8 md:my-12">
-    <button
-      v-for="difficulty in DIFFICULTIES"
-      :key="difficulty"
-      class="select uppercase"
-      :class="{ '!bg-red-100': selectedDifficulty === difficulty }"
-      @click="selectedDifficulty = difficulty"
-    >
-      {{ difficulty }}
-    </button>
-  </div>
+  <section class="relative mb-16">
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 my-8 md:my-12">
+      <button
+        v-for="difficulty in DIFFICULTIES"
+        :key="difficulty"
+        class="select uppercase"
+        :class="{ '!bg-red-100': selectedDifficulty === difficulty }"
+        @click="selectedDifficulty = difficulty"
+      >
+        {{ difficulty }}
+      </button>
+    </div>
 
-  <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 my-8 md:my-12">
-    <button
-      v-for="category, categoryId in CATEGORIES"
-      :key="category"
-      class="select"
-      :class="{ '!bg-red-100': selectedCategory === categoryId }"
-      @click="selectedCategory = categoryId"
-    >
-      {{ category }}
-    </button>
-  </div>
+    <hr class="my-8 border-t-primary-600">
 
-  <div
-    v-show="selectedDifficulty != null && selectedCategory != null"
-    class="flex justify-center items-center"
-  >
-    <NuxtLink class="btn" :to="`/questions/${selectedDifficulty}/${selectedCategory}`">
-      Start
-    </NuxtLink>
-  </div>
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 my-8 md:my-12">
+      <button
+        v-for="category, categoryId in CATEGORIES"
+        :key="category"
+        class="select"
+        :class="{ '!bg-red-100': selectedCategory === categoryId }"
+        @click="selectedCategory = categoryId"
+      >
+        {{ category }}
+      </button>
+    </div>
+
+    <div
+      v-show="selectedDifficulty != null && selectedCategory != null"
+      class="flex justify-center items-center"
+    >
+      <NuxtLink class="btn uppercase fixed bottom-4 shadow-md shadow-black" :to="`/questions/${selectedDifficulty}/${selectedCategory}`">
+        Start
+      </NuxtLink>
+    </div>
+  </section>
 </template>
