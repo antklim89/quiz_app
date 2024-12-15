de1cc6d3298bff3713435d75b39381abd8b45085
Anton <antklim89@mail.ru>
2024-12-15 20:02:18 +0300

    fix: fix link

--- app/components/CategorySelect.vue
+++ app/components/CategorySelect.vue
@@ -28,11 +28,11 @@ const storeArray = computed(() => DIFFICULTIES.map((difficulty) => {
       :key="difficulty"
       class="flex flex-col space-y-2"
     >
-      <NuxtLink v-if="answersLength < AMOUNT" class="btn uppercase" :to="`/questions/easy/${categoryId}?page=${lastAnsweredQuestion}`">
+      <NuxtLink v-if="answersLength < AMOUNT" class="btn uppercase" :to="`/questions/${difficulty}/${categoryId}?page=${lastAnsweredQuestion}`">
         <span>{{ difficulty }} | {{ answersLength }} / {{ AMOUNT }}</span>
       </NuxtLink>
 
-      <NuxtLink v-else class="btn uppercase !bg-green-600 hover:!bg-green-700" :to="`/questions/easy/${categoryId}/results`">
+      <NuxtLink v-else class="btn uppercase !bg-green-600 hover:!bg-green-700" :to="`/questions/${difficulty}/${categoryId}/results`">
         <span>{{ difficulty }} | Show results</span>
       </NuxtLink>
 
