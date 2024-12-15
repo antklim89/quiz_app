3fbac4d0331d8a246eb4574dc335dae5f800545a
Anton <antklim89@mail.ru>
2024-12-15 19:46:19 +0300

    feat: add about page

---
+++ app/components/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <h1 class="text-4xl text-center mb-8">
+      About
+    </h1>
+
+    <pre>
+      This is a simple quiz app.
+      You can select a category and difficulty and answer 10 questions.
+      After that, you can see your results.
+      Good luck!
+    </pre>
+  </section>
+</template>
