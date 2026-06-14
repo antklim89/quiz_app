b22ca80bd0e4799c68a42ff45314661deb34ba9b
Anton <antklim89@mail.ru>
2026-06-14 20:19:37 +0300

    feat: update fetchQuestions
    add errors

--- app/utils/fetches.ts
+++ app/utils/fetches.ts
@@ -4,13 +4,38 @@ import type { QuestionResponse } from '~/types';
 
 export async function fetchQuestions({ difficulty, categoryId }: { difficulty: string; categoryId: string }) {
   const config = useRuntimeConfig();
-  const response = await $fetch<QuestionResponse>(config.public.apiURL, {
-    params: {
-      amount: AMOUNT,
-      difficulty,
-      category: categoryId,
-    },
-  });
-  const fetchedQuestions = questionTransform(response);
-  return fetchedQuestions;
+  try {
+    const response = await fetch(`${config.public.apiURL}?amount=${AMOUNT}&difficulty=${difficulty}&category=${categoryId}`);
+
+    if (!response.ok) {
+      if (response.status === 404) {
+        return {
+          fetchedQuestions: null,
+          error: createError({ message: 'Question not found.', status: response.status }),
+        };
+      }
+      if (response.status === 429) {
+        return {
+          fetchedQuestions: null,
+          error: createError({ message: 'Too many requests. Wait and try again.', status: response.status }),
+        };
+      }
+      return {
+        fetchedQuestions: null,
+        error: createError({ message: 'Unexpected error. Try again later.', status: 500 }),
+      };
+    }
+
+    const responseData = await response.json() as QuestionResponse;
+    const fetchedQuestions = questionTransform(responseData);
+    return {
+      fetchedQuestions,
+      error: null,
+    };
+  } catch {
+    return {
+      fetchedQuestions: null,
+      error: createError({ message: 'Unexpected error. Try again later.', status: 500 }),
+    };
+  }
 }
