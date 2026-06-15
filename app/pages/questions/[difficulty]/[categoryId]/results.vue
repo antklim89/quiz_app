affdc0683e991860dfffaa0cf44a0ea1b955c773
Anton <antklim89@mail.ru>
2026-06-15 20:03:41 +0300

    feat: use useCategoryId and useDifficulty

--- app/pages/questions/[difficulty]/[categoryId]/results.vue
+++ app/pages/questions/[difficulty]/[categoryId]/results.vue
@@ -1,21 +1,6 @@
 <script setup lang="ts">
-import { CATEGORIES, DIFFICULTIES } from '~/constants';
-import type { CategoryId, Difficulty } from '~/types';
-
-
-definePageMeta({
-  validate: ({ params }) => !Number.isNaN(Number(params.page || '1'))
-    && Number(params.page || '1') > 0
-    && Number(params.page || '1') <= 10
-    && typeof params.categoryId === 'string'
-    && typeof params.difficulty === 'string'
-    && Object.keys(CATEGORIES).includes(params.categoryId)
-    && DIFFICULTIES.includes(params.difficulty as Difficulty),
-});
-
-const { params } = useRoute();
-const categoryId = params.categoryId as CategoryId;
-const difficulty = params.difficulty as Difficulty;
+const categoryId = useCategoryId();
+const difficulty = useDifficulty();
 </script>
 
 <template>
