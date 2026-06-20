1cb3f56e7dc915e071fde577d2adcc37f3f3ecf1
Anton <antklim89@mail.ru>
2026-06-20 11:30:43 +0300

    fix(styles): increase grid breakpoints

--- app/components/QuestionsSelect.vue
+++ app/components/QuestionsSelect.vue
@@ -1,6 +1,6 @@
 
 <template>
-  <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 my-8 md:my-12">
+  <section class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 my-8 md:my-12">
     <slot />
   </section>
 </template>
