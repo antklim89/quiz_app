b5e44324dbda7a9b4daae0080fa88bb3599b061f
Anton <antklim89@mail.ru>
2026-06-20 11:57:19 +0300

    fix: return 1 if no page query

--- app/composables/usePage.ts
+++ app/composables/usePage.ts
@@ -4,7 +4,7 @@ export function usePage() {
   const route = useRoute();
 
   const resultPage = computed(() => {
-    if (typeof route.query.page !== 'string') throw new Error('Invalid page query parameter');
+    if (typeof route.query.page !== 'string') return 1;
     const page = Number.parseInt((route.query.page), 10);
     if (Number.isNaN(page) || page < 1 || page > 10) throw new Error('Invalid page query parameter');
     return page;
